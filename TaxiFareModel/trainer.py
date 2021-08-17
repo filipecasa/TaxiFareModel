c26938c641672aa044016bd65a44fb22c359ef38
filipecasa <filipecasa@gmail.com>
2021-08-17 16:11:58 +0200

    Taxi Fare Model

---
+++ TaxiFareModel/trainer.py
@@ -0,0 +1,79 @@
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+import pandas as pd
+import numpy as np
+from data import get_data, clean_data
+from encoders import DistanceTransformer, TimeFeaturesEncoder
+from utils import compute_rmse
+
+
+class Trainer():
+    def __init__(self, X, y):
+        """
+            X: pandas DataFrame
+            y: pandas Series
+        """
+        self.pipe = None
+        self.X = X
+        self.y = y
+
+    def set_pipeline(self):
+        """defines the pipeline as a class attribute"""
+        # create distance pipeline
+        dist_pipe = Pipeline([
+            ('dist_trans', DistanceTransformer()),
+            ('stdscaler', StandardScaler())
+        ])
+        # create time pipeline
+        time_pipe = Pipeline([
+            ('time_enc', TimeFeaturesEncoder('pickup_datetime')),
+            ('ohe', OneHotEncoder(handle_unknown='ignore'))
+        ])
+
+        # create preprocessing pipeline
+        preproc_pipe = ColumnTransformer([
+                ('distance', dist_pipe, ["pickup_latitude", "pickup_longitude", 'dropoff_latitude', 'dropoff_longitude']),
+                ('time', time_pipe, ['pickup_datetime'])
+            ], remainder="drop")
+
+        # Add the model of your choice to the pipeline
+        self.pipe = Pipeline([
+            ('preproc', preproc_pipe),
+            ('linear_model', LinearRegression())
+        ])
+
+
+    def run(self):
+        """set and train the pipeline"""
+        self.set_pipeline()
+        # train the pipelined model
+        self.pipe.fit(self.X, self.y)
+
+
+    def evaluate(self, X_test, y_test):
+        """evaluates the pipeline on df_test and return the RMSE"""
+        # compute y_pred on the test set
+        y_pred = self.pipe.predict(X_test)
+
+        return np.sqrt(((y_pred - y_test)**2).mean())
+
+
+if __name__ == "__main__":
+    # get data
+    df = get_data()
+    # clean data
+    df_clean = clean_data(df)
+    # set X and y
+    X = df_clean.drop("fare_amount", axis=1)
+    y = df_clean["fare_amount"]
+    # hold out
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.15)
+    # train
+    trainer = Trainer(X = X_train, y = y_train)
+    trainer.run()
+    # evaluate
+    rmse = trainer.evaluate(X_test, y_test)
+    print(rmse)
